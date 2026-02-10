365e950529f5cf3a9fd5a51f2e5b0ad9dda87c19
jordan <nicholsonjordan48@gmail.com>
2026-02-09 19:18:40 -0500

    test commit

---
+++ src/Window.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame {
+    
+    public Window() {
+        
+        JFrame jordanWindow = new JFrame();
+        jordanWindow.setVisible(true);
+
+        jordanWindow.setSize(400, 500);
+        jordanWindow.setTitle("Concert Ticket Calculator");
+
+       
+    }
+}
