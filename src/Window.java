b70d27a783470e176868df370bd1c7f7dae8a32b
jordan <nicholsonjordan48@gmail.com>
2026-04-23 16:37:46 -0400

    commit

--- src/Window.java
+++ src/Window.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import net.miginfocom.swing.MigLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.KeyEvent; 
 import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
